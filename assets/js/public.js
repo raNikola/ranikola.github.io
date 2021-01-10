5fb48fac480ed8f4ce6571cbc04ca098dfc43200
raNikola <nrandjelovic@bettercollective.com>
2021-01-10 01:06:06 +0100

    - Add menu - Add back to top btn - Add animation to topBtn - Add JS trigger on scroll
    Took 4 hours 38 minutes

--- assets/js/public.js
+++ assets/js/public.js
@@ -1,7 +1,30 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js',  'assets/particles.json');
+particlesJS.load('particles-js', 'assets/particles.json');
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.tooltipped');
-    var instances = M.Tooltip.init(elems);
+document.addEventListener('DOMContentLoaded', function () {
+  const toolTipped = document.querySelectorAll('.tooltipped');
+  const instances = M.Tooltip.init(toolTipped);
+
+  const nav = document.querySelectorAll('.sidenav');
+  const navInstances = M.Sidenav.init(nav);
+
+
+});
+
+document.addEventListener('scroll', function (e) {
+  const topBtn = document.querySelector('.top-btn');
+  const mainMenu = document.querySelector('.navbar__main');
+
+  if ((window.scrollY - 150) > 0) {
+    topBtn.style.opacity = '1';
+    topBtn.style.visibility = 'visible';
+
+    mainMenu.classList.add('navbar-fixed');
+    mainMenu.classList.remove('navbar-static');
+  } else {
+    topBtn.style.opacity = '0';
+    topBtn.style.visibility = 'hidden';
+    mainMenu.classList.add('navbar-static');
+    mainMenu.classList.remove('navbar-fixed');
+  }
 });
